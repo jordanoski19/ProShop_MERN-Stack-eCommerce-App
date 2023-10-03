d97721ffb60c5295bb8f235e3ca449c6c7842aec
Filip <boopoup@gmail.com>
2023-10-03 09:55:02 +0200

    Implement React Router & Link and LinkContainer

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import Product from "../components/Products";
+import Product from "../components/Product";
 import products from "../products";
 
 const HomeScreen = () => {
