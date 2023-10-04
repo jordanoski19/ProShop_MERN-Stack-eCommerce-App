e4160a220ebfce96c407f19574b95139b06c46fe
Filip <boopoup@gmail.com>
2023-10-05 00:01:12 +0200

    Express setup with Nodemon & Concurrently

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import products from "./data/products.js";
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const productId = products.find((item) => item._id === req.params.id);
+  res.json(productId);
+});
+
+app.listen(port, () => console.log(`port is running on ${port}`));
