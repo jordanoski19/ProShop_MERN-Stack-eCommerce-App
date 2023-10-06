ff7189ce3f74887ed6281f2f5018beaff04e6493
Filip <boopoup@gmail.com>
2023-10-06 07:30:00 +0200

    Redux store, apiSlice & productSlice

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,29 @@
-import React from "react";
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Product from "../components/Product";
-import axios from "axios";
 
 const HomeScreen = () => {
-  const [products, setProdutcs] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProdutcs(data);
-    };
-
-    fetchProducts();
-  });
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
-    <h1>
-      <Row>
-        {products.map((item) => (
-          <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={item}></Product>
-          </Col>
-        ))}
-      </Row>
-    </h1>
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error?.data.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
+    </>
   );
 };
 
