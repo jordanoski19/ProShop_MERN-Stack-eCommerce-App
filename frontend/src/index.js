2520b604d45f1d35116756bca87ce1f44318e0e5
Filip <boopoup@gmail.com>
2023-10-09 01:42:51 +0200

    Add and remove from cart & Checkout handler

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
